ec55b4f32dc1975982be222573f82d08313bd0aa
William Wang <61264396+william-ying@users.noreply.github.com>
2025-12-04 18:50:23 -0800

    Update hash.h

--- hash.h
+++ hash.h
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <random>
 #include <chrono>
+#include <cctype>
 
 typedef std::size_t HASH_INDEX_T;
 
@@ -20,15 +21,28 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
-
-
+        HASH_INDEX_T ret = 0;
+        int counter = 0;
+        for (int i = k.size() - 1; i >= 0; i -= 6) {
+            unsigned long long pos = 0;
+            for (int j = 5; j >= 0; j--) {
+                if (i - j >= 0) {
+                    pos = pos * 36;
+                    pos += (letterDigitToNumber(k[i - j]));
+                }
+            }
+            ret += pos * rValues[4 - counter];
+            counter++;
+        }
+        return ret;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
-
+        if (isalpha(letter)) {return std::tolower(letter) - 96;}
+        else {return atoi(letter);}
     }
 
     // Code to generate the random R values
